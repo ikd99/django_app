fd1a808bd2a3ee1e07820297634770d9562a7308
増田裕太 <massu2000.1019@gmail.com>
2021-10-02 15:40:06 +0900

    🗃️ DBのフィールド、オプションの設定変更

--- main/models.py
+++ main/models.py
@@ -1,26 +1,27 @@
 from django.db import models
+from django.db.models.expressions import F
 from django.db.models.fields import IntegerField
 
 # Create your models here.
 class user_info(models.Model):
-  user_name = models.CharField(max_length=20)
+  user_name = models.CharField(max_length=20, null=False)
   is_driver = models.BooleanField(default=False, null=False)
-  hashed_pass = models.CharField(max_length=50)
+  hashed_pass = models.IntegerField(null=False)
   region = models.CharField(max_length=50, null=False)
   total_socore = models.FloatField(max_length=50)
 
 class request(models.Model):
   user_id = models.IntegerField()
-  title = models.CharField(max_length=100)
-  matching_complete = models.IntegerField(default=0)
-  request_complete = models.IntegerField(default=0)
-  share_or_not = models.IntegerField(default=0)
-  post_time = models.DateTimeField()
+  title = models.CharField(max_length=100, null=False)
+  matching_complete = models.BooleanField(default=False)
+  request_complete = models.BooleanField(default=False)
+  share_or_not = models.BooleanField(default=False)
+  post_time = models.DateTimeField(null=False)
   text = models.TextField(max_length=1000, null=False)
-  departure_place = models.CharField(max_length=100)
-  destination_place = models.CharField(max_length=100)
+  departure_place = models.CharField(max_length=100, null=False)
+  destination_place = models.CharField(max_length=100, null=False)
   delivery_date = models.CharField(max_length=50)
-  asking_price = models.IntegerField()
+  asking_price = models.IntegerField(null=False)
   driver_evaluation = models.FloatField()
   client_evaluation = models.FloatField()
 
@@ -30,7 +31,7 @@ class message(models.Model):
   text = models.TextField(max_length=1000, null=False)
 
 class payment(models.Model):
-  post_id = models.IntegerField()
+  post_id = models.IntegerField(null=False)
   payment_amount = models.IntegerField(null=False)
 
 class favorite(models.Model):
