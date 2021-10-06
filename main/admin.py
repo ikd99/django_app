358abf885d379f0f2ddb7d725d59317c08aec277
ittyi#日本語ファイル名がエスケープされないように <vioiv.k4@gmail.com>
2021-10-06 20:57:20 +0900

    -DBtool adminの設定

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from main.models import user_info, request, message, payment, favorite
+
+admin.site.register(user_info)
+admin.site.register(request)
+admin.site.register(message)
+admin.site.register(payment)
+admin.site.register(favorite)
